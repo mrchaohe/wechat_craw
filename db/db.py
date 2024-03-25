089650fa91de4c4ca06de991cb167fce248919d9
‘zhounana’ <1140733793@qq.com>
2024-03-25 18:06:12 +0800

    add sqlite alchemy

---
+++ db/db.py
@@ -0,0 +1,71 @@
+
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Boolean,Column,Integer,String, TIMESTAMP, ForeignKey
+
+# from db.sqlite import engine
+from sqlalchemy import create_engine
+
+
+SQLiteURL = 'sqlite:///wechat.db'
+# 创建engine，即数据库驱动信息
+engine = create_engine(
+    url=SQLiteURL,
+    echo=True,    # 打开sqlalchemy ORM过程中的详细信息
+    connect_args={
+        'check_same_thread':False   # 是否多线程
+    }
+)
+
+
+
+# 数据表的基类（定义表结构用）
+Base = declarative_base()
+
+
+class Account(Base):
+    # 公众号表
+    __tablename__='account'
+    id = Column(Integer, primary_key=True, autoincrement=True,comment="公众号id")
+    fake_id = Column(String(32),index=True,comment="公众号唯一标识，系统给定")
+    name = Column(String(32),unique=True,index=True,comment="公众号名称")
+    create_time = Column(TIMESTAMP,comment="在系统添加时间")
+ 
+
+class Article(Base):
+    # 文章链接表
+    __tablename__='article'
+    aid = Column(String(32),primary_key=True,index=True,comment="文章id,具有唯一标识")
+    pid = Column(Integer, ForeignKey('account.id'), comment="公众号id")
+    name = Column(String(128),comment="文章标题")
+    link = Column(String(256),comment="文章链接")
+    cover = Column(String(256),comment="封面链接")
+    status = Column(Integer,comment="状态 0 未下载 1下载完成")
+    create_time = Column(TIMESTAMP,comment="记录创建时间")
+    update_time = Column(TIMESTAMP,comment="记录更新时间")
+ 
+
+class TaskStatus(Base):
+    # 下载任务状态表
+    __tablename__='task_status'
+    id = Column(Integer, primary_key=True, autoincrement=True,comment="任务id")
+    pid = Column(Integer, ForeignKey('account.id'),comment="公众号id")
+    start_time = Column(TIMESTAMP,comment="任务开始时间")
+    end_time = Column(TIMESTAMP,comment="任务结束时间")
+    status = Column(Integer,comment="任务状态0 待运行 1运行中 2 运行完成")
+    pagenum = Column(Integer,comment="上次运行页码")
+    link_cnt = Column(Integer,comment="上次记录的文章链接数")
+    down_cnt = Column(Integer,comment="上次下载的文章数")
+    total  = Column(Integer,comment="上次的文章总数")
+    create_time = Column(TIMESTAMP,comment="任务创建时间")
+    update_time = Column(TIMESTAMP,comment="任务修改时间")
+
+
+# 创建表
+# checkfirst=True 默认也是 True，即如果数据库存在则不再创建
+Base.metadata.create_all(engine, checkfirst=True)
+
+# session 与数据库交互
+Session = sessionmaker(bind=engine) 
+session = Session() 
--- readme.md
+++ readme.md
@@ -9,20 +9,20 @@ public_account
 
 |   id  | name  | fakeid | create_time | bak |
 |  ---- | ----  | ----   | ----       | ----  | 
-|自增id | 公众号名|唯一标识| 创建时间|备用字段|
+|自增id int| 公众号名 string|唯一标识 string| 创建时间|备用字段|
 
 
 2. 文章下载表
 
 article
 
-|   id  | p_id  | name | link | status | create_time | update_time | 
+|   aid  | p_id  | name | link | status | create_time | update_time | 
 |  ---- | ----  | ----   | ----       | ----  |  ----   | ----   
-|自增id | 公众号id| 名称| 链接|状态 0 未下载 1下载完成| 创建时间| 修改时间|
+|公众号文章自身id | 公众号id| 名称| 链接|状态 0 未下载 1下载完成| 创建时间| 修改时间|
 
 
 3. 下载状态表
 
  | p_id  | start_time | end_time | status | pagenum | link_cnt | down_cnt | total| 
  | ----  | ----   | ----       | ----  |  ----   | ----   |  ----   | ---- |
-| 公众号id| 开始时间| 结束时间|状态 0 待执行 1 执行完成| 页码| 创建链接数| 下载数| 总数|
+| 公众号id| 开始时间| 结束时间|状态 0 待执行 1 执行中 2执行完成| 页码| 创建链接数| 下载数| 总数|
